adae2399b28799e8ef0464fd2e75c41e20b2c455
Dave Lawson <drlawson@gmail.com>
2021-05-29 00:41:40 -0400

    Updated passthrough for img & css

--- .eleventy.js
+++ .eleventy.js
@@ -57,8 +57,9 @@ module.exports = function(eleventyConfig) {
   });
 
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("_src/img");
+  eleventyConfig.addPassthroughCopy("_src/css");
+
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
